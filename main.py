c0ec8570113e45a35bf822a0752d9b8dbfc4c10f
anandneredimilli <paulanand926@gmail.com>
2023-06-19 00:23:04 +0530

    protected api key using env var

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import requests
+import os
 
-url = "https://weatherapi-com.p.rapidapi.com/current.json"
+api_end_point = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = os.environ.get('Api_key')
+# print(os.environ.get('Api_key'))
 
-parameters = {"q":"16.518854,81.368480"} #latitude and longitude
+location = {'lat':16.518854,
+            'lon':81.368480,
+            'appid':api_key,
+            'exclude':'current,minutely,daily'}
+data = requests.get(url=api_end_point, params=location)
+# print(data.status_code)
+hourly_weather_data = data.json()
+will_it_rain = False
+for i in range(0,12):
+    condition_code = hourly_weather_data['hourly'][i]['weather'][0]['id']
+    
+    if 200 <= condition_code <700 or 801 <= condition_code <=804:
+        will_it_rain = True
 
-key = {
-	"X-RapidAPI-Key": "8c5205b29emshd4249e142e5330dp1fe9e0jsn4becbc3de772",
-	"X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-}
+if will_it_rain:
+    print("chances of rain, please carry umbrella")
 
-response = requests.get(url, headers=key, params=parameters)
 
-print(response.json())
