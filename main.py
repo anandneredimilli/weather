1a5a8ca4ea207aeb1d00e56c9b2139def88ccb0a
anandneredimilli <paulanand926@gmail.com>
2023-06-20 17:02:54 +0530

    imported sendmail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import sendmail
 
 api_end_point = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = os.environ.get('Api_key')
@@ -10,7 +11,7 @@ location = {'lat':16.518854,
             'appid':api_key,
             'exclude':'current,minutely,daily'}
 data = requests.get(url=api_end_point, params=location)
-# print(data.status_code)
+print(data.status_code)
 hourly_weather_data = data.json()
 will_it_rain = False
 for i in range(0,12):
@@ -20,6 +21,4 @@ for i in range(0,12):
         will_it_rain = True
 
 if will_it_rain:
-    print("chances of rain, please carry umbrella")
-
-
+    sendmail = sendmail.SendMail(message='chances of rain please carry umbrella')
